223bd91ac1634f3f3837305886c9e05d9959de16
omar khaled <omarkhald02@gmail.com>
2023-07-02 18:52:29 +0300

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ df_agg_sub = pd.read_csv('Aggregated_Metrics_By_Country_And_Subscriber_Status.cs
 df_agg.columns = ['Video','Video title','Video publish time','Comments added','Shares','Dislikes','Likes',
                       'Subscribers lost','Subscribers gained','RPM(USD)','CPM(USD)','Average % viewed','Average view duration',
                       'Views','Watch time (hours)','Subscribers','Your estimated revenue (USD)','Impressions','Impressions ctr(%)']
-df_agg['Video publish time'] = pd.to_datetime(df_agg['Video publish time'],format='mixed')
+df_agg['Video publish time'] = pd.to_datetime(df_agg['Video publish time'],format='%d/%m/%y %H:%M:%S.%f' )
 #df_agg['Video publish time'] = df_agg['Video publish time'].apply(lambda x: datetime.strptime(x,'%d/%m/%y %H:%M:%S.%f'))
 df_agg['Average view duration'] = df_agg['Average view duration'].apply(lambda x: datetime.strptime(x,'%H:%M:%S'))
 df_agg['Avg_duration_sec'] = df_agg['Average view duration'].apply(lambda x: x.second + x.minute*60 + x.hour*3600)
